7ec191910e3a92e96f3e07bd1a7104f2edc32c2a
Joao Gabriel Motta <jgmotta2004@gmail.com>
2026-01-20 15:49:31 -0300

    Refactor SignIn component to integrate Zod validation, enhance error handling, and improve form action management.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,16 +1,44 @@
 import { useActionState } from "react";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+import { api } from "../services/api";
+
+const signInSchema = z.object({
+  email: z.string().email({ message: "E-mail inválido!" }),
+  password: z.string().trim().min(1, { message: "Informe a senha!" }),
+});
 
 export default function SignIn() {
-  function onAction(formData: FormData) {
-    console.log(formData.get("email"));
+  const [state, formAction, isLoading] = useActionState(onAction, null);
 
-    alert("Enviado!");
+  async function onAction(prevState: any, formData: FormData) {
+    try {
+      const data = signInSchema.parse({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      });
+
+      const response = await api.post("/sessions", data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof ZodError) {
+        return { message: error.issues[0].message };
+      }
+
+      if (error instanceof AxiosError) {
+        return { message: error.response?.data.message };
+      }
+
+      return { message: "Não foi possível fazer login" };
+    }
   }
 
   return (
-    <form action={onAction} className="w-full flex flex-col gap-4">
+    <form action={formAction} className="w-full flex flex-col gap-4">
       <Input
         required
         name="email"
@@ -26,6 +54,8 @@ export default function SignIn() {
         placeholder="123456"
         type="password"
       />
+
+      <span className="text-xs text-red-600 text-center">{state?.message}</span>
 
       <Button isLoading={isLoading} type="submit">
         Entrar
